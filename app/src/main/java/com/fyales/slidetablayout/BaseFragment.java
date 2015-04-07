c6defd89d4a0784e005cc979d73b62839765f3fe
fyales <sww201006@126.com>
2015-04-07 17:53:37 +0800

    完善demo

--- app/src/main/java/com/fyales/slidetablayout/BaseFragment.java
+++ app/src/main/java/com/fyales/slidetablayout/BaseFragment.java
@@ -5,14 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * @author fyales
  */
 public class BaseFragment extends Fragment {
+    private static final String DATA = "data";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment,container,false);
+        View view = inflater.inflate(R.layout.fragment,container,false);
+        TextView textView = (TextView)view.findViewById(R.id.text);
+        textView.setText("Fragment#" + getArguments().getInt(DATA,0));
+        return textView;
     }
 
     @Override
@@ -23,7 +29,7 @@ public class BaseFragment extends Fragment {
     public static Fragment newInstance(int type){
         Fragment fragment = new BaseFragment();
         Bundle bundle = new Bundle();
-        bundle.putInt("DATA",type);
+        bundle.putInt(DATA,type);
         fragment.setArguments(bundle);
         return fragment;
     }
